0838f3ff98919b7a5d7e0454f067265866157f6f
Yatin Kapoor <yatinkapoor200@gmail.com>
2024-03-23 19:46:52 +0530

    Sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -7,10 +7,12 @@ import { useState } from "react";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/usePlatforms";
 import { Game } from "./hooks/useGames";
+import SortSelector from "./components/SortSelector";
 
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 function App() {
@@ -39,15 +41,16 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <PlatformSelector
-          selectedPlatform={gameQuery.platform}
-          onSelectPlatform={(platform) =>
-            setGameQuery({ ...gameQuery, platform })
-          }
-        />
-        <GameGrid
-          gameQuery={gameQuery}
-        />
+        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
+          <PlatformSelector
+            selectedPlatform={gameQuery.platform}
+            onSelectPlatform={(platform) =>
+              setGameQuery({ ...gameQuery, platform })
+            }
+          />
+          <SortSelector onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})} sortOrder={gameQuery.sortOrder}/>
+        </HStack>
+        <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,9 +20,11 @@ export interface Game {
 const useGames = (gameQuery: GameQuery) => 
   useData<Game>('/games', { 
   params: {
-    genres: gameQuery.genre?.id, parent_platforms: gameQuery.platform?.id
+    genres: gameQuery.genre?.id, 
+    parent_platforms: gameQuery.platform?.id, 
+    ordering: gameQuery.sortOrder
   }}, 
-  [gameQuery.genre?.id, gameQuery.platform?.id]
+  [gameQuery]
   )
 
 export default useGames;
