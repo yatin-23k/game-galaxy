4cda8aa29623d5a68f3f3198de9560c01a2825af
Yatin Kapoor <yatinkapoor200@gmail.com>
2024-05-09 13:40:07 +0530

    Refactor- simplify game query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,11 +4,12 @@ import usePlatform, { Platform } from '../hooks/usePlatforms';
 
 interface Props {
     onSelectPlatform: (platform: Platform) => void;
-    selectedPlatform: Platform | null;
+    selectedPlatformId?: number;
 }
 
-const PlatformSelector = ({onSelectPlatform, selectedPlatform}: Props) => {
+const PlatformSelector = ({onSelectPlatform, selectedPlatformId}: Props) => {
     const {data, error} = usePlatform();
+    const selectedPlatform = data?.results.find(p => p.id === selectedPlatformId)
 
     if(error) return null;
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,8 +22,8 @@ const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>,
     apiClient
       .getAll({
         params: {
-          genres: gameQuery.genre?.id, 
-          parent_platforms: gameQuery.platform?.id, 
+          genres: gameQuery.genreId, 
+          parent_platforms: gameQuery.platformId, 
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
           page: pageParam
