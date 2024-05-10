3b3cd63e9647056cba6b3aef371434e3cec1a2e6
Yatin Kapoor <yatinkapoor200@gmail.com>
2024-05-10 22:45:34 +0530

    Build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -6,6 +6,7 @@ import DefinitionItem from "../components/DefinitionItem";
 import platforms from "../data/platforms";
 import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -20,6 +21,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
