845e42f0f28aba7f3692ae11d5cec7b9fa23f6f3
Yatin Kapoor <yatinkapoor200@gmail.com>
2024-05-14 08:58:10 +0530

    Error fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
-import Homepage from "./pages/Homepage";
+import Homepage from "./pages/HomePage";
 import GameDetailPage from "./pages/GameDetailPage";
 import ErrorPage from "./pages/ErrorPage";
 
