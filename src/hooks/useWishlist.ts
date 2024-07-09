d95c203a06cb32625a5954619fdc9557479b2bf6
Yatin Kapoor <yatinkapoor200@gmail.com>
2024-07-10 04:31:53 +0530

    add: wishlist page logic

--- src/hooks/useWishlist.ts
+++ src/hooks/useWishlist.ts
@@ -1,15 +1,46 @@
 import { useQuery } from "@tanstack/react-query";
 import server from "../services/server-client";
+import APIClient from "../services/api-client";
+import Game from "../entities/Game";
+
+// const useWishlist = () =>
+//   useQuery({
+//     queryKey: ["wishlist"],
+//     queryFn: () =>
+//       server
+//         .get("/wishlist", {
+//           headers: { username: localStorage.getItem("username") },
+//         })
+//         .then((res) => res.data),
+//   });
+  
+const apiClient = new APIClient<Game>("/games");
 
 const useWishlist = () =>
   useQuery({
     queryKey: ["wishlist"],
-    queryFn: () =>
-      server
-        .get("/wishlist", {
-          headers: { username: localStorage.getItem("username") },
+    queryFn: async () => {
+      const res = await server.get("/wishlist", {
+        headers: { username: localStorage.getItem("username") },
+      });
+
+      const items = res.data;
+
+      console.log(items);
+
+      // get game details for each game in wishlist_items
+      const games = await Promise.all(
+        items.map(async (item: { game_id: any; }) => {
+          const slug = item.game_id;
+          const res = await apiClient.get(slug);
+          return res;
         })
-        .then((res) => res.data),
-  });
+      );
+
+      console.log(games);
+
+      return games;
+    },
+});
 
 export default useWishlist;
--- src/pages/WishlistPage.tsx
+++ src/pages/WishlistPage.tsx
@@ -1,9 +1,48 @@
+import { Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import GameCard from "../components/GameCard";
+import GameCardSkeleton from "../components/GameCardSkeleton";
+import Game from "../entities/Game";
 import useWishlist from  "../hooks/useWishlist"
 
 const WishlistPage = () => {
-  const wishlist = useWishlist();
-  console.log(wishlist.data);
-  return <div>WishlistPage</div>;
+   // const server_wishlist = useWishlist();
+   const { data: games, isLoading, error } = useWishlist();
+
+   const skeletons = [
+     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+   ];
+ 
+   if (error) throw error;
+   if (!isLoading && !games) return <Text>No games found in this category</Text>;
+ 
+   return (
+     <div>
+       <Flex
+         // p={{ base: 4, md: 10 }}
+         p={10}
+         w="100%"
+         direction="column"
+       >
+         <Heading as="h1" fontSize="6xl" paddingLeft={10}>
+           Wishlist
+         </Heading>
+         <SimpleGrid
+           columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+           padding={10}
+           spacing={5}
+         >
+           {isLoading &&
+             skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+           {!isLoading && games?.length === 0 && (
+             <Text>No games found in this category</Text>
+           )}
+           {games?.map((game: Game) => (
+             <GameCard key={game.slug} game={game} />
+           ))}
+         </SimpleGrid>
+       </Flex>
+     </div>
+   );
 };
 
 export default WishlistPage;
