b5d898fd95069f0f1a108aeb8501f40e8b05b9a6
Yatin Kapoor <yatinkapoor200@gmail.com>
2024-07-22 21:16:26 +0530

    update game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,10 +1,19 @@
-import { Card, CardBody, Flex, HStack, Heading, Image } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  Flex,
+  HStack,
+  Heading,
+  Image,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import Game from "../entities/Game";
 import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
 import Emoji from "./Emoji";
 import PlatformIconList from "./PlatformIconList";
+import WishlistButton from "./WishlistButton";
 
 interface Props {
   game: Game;
@@ -14,21 +23,31 @@ const GameCard = ({ game }: Props) => {
   return (
     <Card height="100%">
       <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent="space-between" marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
+      <Flex direction="column" height="100%">
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Link to={"/games/" + game.slug}>
+            {<Heading fontSize="2xl">{game.name}</Heading>}
+          </Link>
+          <Emoji rating={game.rating_top} />
+        </CardBody>
+        <HStack justifyContent="space-between" paddingX={4} paddingBottom={4}>
+          <WishlistButton gameSlug={game.slug} />
+          <Button
+            colorScheme="yellow"
+            onClick={() => {alert("This functionality is currently under development. Please check back later!")}}
+          >
+            Buy
+          </Button>
         </HStack>
-
-        <Link to={"/games/" + game.slug}>
-          {<Heading fontSize="2xl">{game.name}</Heading>}
-        </Link>
-        <Emoji rating={game.rating_top} />
-      </CardBody>
+      </Flex>
     </Card>
-  );
+  )
 };
 
 export default GameCard;
